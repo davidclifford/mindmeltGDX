b197b92de5cab77d04b2bd7f0845641f4c8dc9bb
David Clifford <david.clifford.142@gmail.com>
2017-05-04 00:13:36 +1000

    Add some GUI code

--- core/src/mindmelt/game/MindmeltGDX.java
+++ core/src/mindmelt/game/MindmeltGDX.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -31,6 +32,9 @@ public class MindmeltGDX extends Game {
 
 		tileImages = new Texture("image/tiles.png");
 		tile = TextureRegion.split(tileImages, 32, 32);
+
+		Pixmap px = new Pixmap(Gdx.files.internal("image/pointer.png"));
+		Gdx.graphics.setCursor(Gdx.graphics.newCursor(px,0,0));
 
 		startScreen = new Texture("image/start.bmp");
 		setScreen(new StartScreen(this));
